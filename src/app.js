2d64db0e72c7826ea371b7dcd9e8317893fe3c44
SimonHotDog <simon.lu@live.se>
2022-01-10 03:45:23 +0100

    Static content is now rendered with handlebars

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
 const http = require('http');
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const port = process.env.PORT || 3000
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Aeolus'
+    })
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard', {
+        title: 'Aeolus - Dashboard'
+    })
+})
+
+app.get('/admin', (req, res) => {
+    res.render('admin', {
+        title: 'Aeolus - Admin'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
