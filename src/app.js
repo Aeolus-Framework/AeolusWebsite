55ddba564ea47a81127923343fad35948b00028b
SimonHotDog <simon.lu@live.se>
2022-01-10 18:02:44 +0100

    Changed navbar to be a partial

--- src/app.js
+++ src/app.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || 5500
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
