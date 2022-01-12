2dcd248a9bb5ad5f3ef55ba10729f154e3cb12c1
SimonHotDog <simon.lu@live.se>
2022-01-12 14:34:47 +0100

    Why won't google sign in give a response?

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
 const userRouter = require('./routes/user-route')
+const oauth = require('./routes/oauth')
 
 const app = express()
 
 const port = process.env.PORT || 5500
 
 app.use(userRouter)
+app.use(oauth)
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
